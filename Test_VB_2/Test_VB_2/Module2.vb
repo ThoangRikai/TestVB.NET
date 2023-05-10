210f73f8e33c5ff3102109113b32cab2c6f3952d
ThoangRikai <thoang.tran@rikai.technology>
2023-05-10 17:39:31 +0700

    Dimensional Array and Jagged Array in VB.NET

--- Test_VB_2/Test_VB_2/Module2.vb
+++ Test_VB_2/Test_VB_2/Module2.vb
@@ -35,22 +35,51 @@
 
         'Array Class
 
-        Dim arr As Integer() = New Integer(5) {3, 6, 2, 5, 1, 4}
-        Console.WriteLine("---Initial Elements---")
-        For Each i As Integer In arr
-            Console.WriteLine(i)
+        'Dim arr As Integer() = New Integer(5) {3, 6, 2, 5, 1, 4}
+        'Console.WriteLine("---Initial Elements---")
+        'For Each i As Integer In arr
+        '    Console.WriteLine(i)
+        'Next
+        'Array.Sort(arr)
+        'Console.WriteLine("---Elements After Sort---")
+        'For Each i As Integer In arr
+        '    Console.WriteLine(i)
+        'Next
+        'Array.Reverse(arr)
+        'Console.WriteLine("---Elements After Reverse---")
+        'For Each i As Integer In arr
+        '    Console.WriteLine(i)
+        'Next
+        'Console.WriteLine("Press Enter Key to Exit")
+        'Console.ReadLine()
+
+        'Dimensional Array
+
+        'Dim array2D As Integer(,) = New Integer(2, 1) {{4, 5}, {5, 0}, {3, 1}}
+        'For i As Integer = 0 To 3 - 1
+        '    For j As Integer = 0 To 2 - 1
+        '        Console.WriteLine("a[{0}, {1}] = {2}", i, j, array2D(i, j))
+        '    Next
+        'Next
+        'Console.WriteLine("Press Enter Key to Exit")
+        'Console.ReadLine()
+
+        'Jagged Array
+
+        Dim jarray As Integer()() = New Integer(2)() {}
+        jarray(0) = New Integer(4) {1, 2, 3, 4, 5}
+        jarray(1) = New Integer(2) {10, 20, 30}
+        jarray(2) = New Integer() {12, 50, 60, 70, 32}
+        Console.WriteLine("---Jagged Array with Single Dimensional Elements---" & vbLf)
+        For i As Integer = 0 To jarray.Length - 1
+            Console.Write("Element[{0}]: ", i)
+            For j As Integer = 0 To jarray(i).Length - 1
+
+                Console.Write("{0}{1}", jarray(i)(j), If(j = (jarray(i).Length - 1), "", " "))
+            Next
+            Console.WriteLine()
         Next
-        Array.Sort(arr)
-        Console.WriteLine("---Elements After Sort---")
-        For Each i As Integer In arr
-            Console.WriteLine(i)
-        Next
-        Array.Reverse(arr)
-        Console.WriteLine("---Elements After Reverse---")
-        For Each i As Integer In arr
-            Console.WriteLine(i)
-        Next
-        Console.WriteLine("Press Enter Key to Exit")
+        Console.WriteLine(vbLf & "Press Enter Key to Exit..")
         Console.ReadLine()
     End Sub
 
