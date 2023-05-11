72f19d6fb2366b553ca2b0088963f40653a70e8d
ThoangRikai <thoang.tran@rikai.technology>
2023-05-11 18:57:49 +0700

    Method - Me Keyword - Constant in VB.NET

--- Test_VB_3/Test_VB_3/Module2.vb
+++ Test_VB_3/Test_VB_3/Module2.vb
@@ -1,45 +1,63 @@
 ﻿Module Module2
-    Class User
 
-        ' Private Constructor
+    'Method
 
-        Private Sub New()
+    'Sub Main()
+    '    Dim result As String = GetUserDetails("Thoang Tran", 23)
+    '    Console.WriteLine(result)
+    '    GetDetails()
+    '    Console.ReadLine()
+    'End Sub
+    'Public Sub GetDetails()
+    '    Console.WriteLine("Press Key Enter to Exit...")
+    'End Sub
+    'Public Function GetUserDetails(ByVal name As String, ByVal age As Integer) As String
+    '    Dim info As String = String.Format("Name: {0}, Age: {1}", name, age)
+    '    Return info
+    'End Function
 
-            Console.WriteLine("I am Private Constructor")
+    'Me Keyword
 
-        End Sub
+    'Class User
+    '    Public name, location As String
+    '    Public marks As Long = 470
+    '    Public Sub New(ByVal name As String, ByVal location As String)
+    '        Me.name = name
+    '        Me.location = location
+    '    End Sub
+    '    Public Sub GetUserDetails()
+    '        Console.WriteLine("Name: {0}", name)
+    '        Console.WriteLine("Location: ", location)
+    '        ' Passing a class instance to the method using this
+    '        Console.WriteLine("Marks: {0}", Exams.GetPercentage(Me)
+    '    End Sub
+    'End Class
 
-        Public Shared name, location As String
+    'Class Exams
+    '    Public Shared Function GetPercentage(ByVal u As User) As Double
+    '        Dim i As Double = (CDbl(470) / 600) * 100
+    '        Return i
+    '    End Function
+    'End Class
 
-        ' Default Constructor
+    'Sub Main()
+    '    Dim u As User = New User("Thoang Tran", "VietNam")
+    '    u.GetUserDetails()
+    '    Console.WriteLine("Press Enter Key to Exit..")
+    '    Console.ReadLine()
+    'End Sub
 
-        Public Sub New(ByVal a As String, ByVal b As String)
 
-            name = a
-
-            location = b
-
-        End Sub
-
-    End Class
-
+    'Const
     Sub Main()
-
-        ' The following comment line will throw an error because constructor is inaccessible
-
-        Dim user As User = New User()
-
-
-
-        ' Only Default constructor with parameters will invoke
-
-        Dim user1 As User = New User("Suresh Dasari", "Hyderabad")
-
-        Console.WriteLine(User.name & ", " & User.location)
-
+        ' Constant variables
+        Const name As String = "Thoang Tran"
+        Const location As String = "Viet Nam"
+        Const age As Integer = 23
+        Console.WriteLine("Name: {0}", name)
+        Console.WriteLine("Location: {0}", location)
+        Console.WriteLine("Age: {0}", age)
         Console.WriteLine("Press Enter Key to Exit..")
-
         Console.ReadLine()
-
     End Sub
 End Module
