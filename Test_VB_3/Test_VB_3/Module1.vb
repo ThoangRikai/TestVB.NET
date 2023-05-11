22ab8ff43fe80f2c62e2fa30939b742b009fa696
ThoangRikai <thoang.tran@rikai.technology>
2023-05-11 16:29:56 +0700

    Constructor - Destructor in VB.NET

---
+++ Test_VB_3/Test_VB_3/Module1.vb
@@ -0,0 +1,96 @@
+﻿Module Module1
+
+    'Class and Object
+
+    'Sub Main()
+    '    Dim user As User = New User("Thoang Tran", 23)
+    '    user.GetUserDetails()
+    '    Console.WriteLine("Press Enter Key to Exit....")
+    '    Console.ReadLine()
+    'End Sub
+
+    'Public Class User
+    '    Public Property UName As String
+    '    Public Property UAge As Integer
+    '    Public Sub New(ByVal name As String, ByVal age As Integer)
+    '        UName = name
+    '        UAge = age
+    '    End Sub
+    '    Public Sub GetUserDetails()
+    '        Console.WriteLine("Name: {0}, Age: {1}", UName, UAge)
+    '    End Sub
+    'End Class
+
+    'Constructor
+
+    'Class User
+    '    Public name, location As String
+    '    'Default Constructor
+
+    '    Public Sub New()
+    '        name = "thoang tran"
+    '        location = "viet nam"
+    '    End Sub
+
+    '    'Parameterized Constructor
+    '    Public Sub New(ByVal a As String, ByVal b As String)
+    '        name = a
+    '        location = b
+    '    End Sub
+    'End Class
+
+    'Sub Main()
+    '    ' Default Constructor will be called
+
+    '    Dim user As User = New User()
+
+    '    ' Parameterized Constructor will be called
+
+    '    Dim user1 As User = New User("Thoang Tran", "Viet Nam")
+    '    Console.WriteLine(user.name & ", " & user.location)
+    '    Console.WriteLine(user1.name & ", " & user1.location)
+    '    Console.WriteLine(vbLf & "Press Enter Key to Exit...")
+    '    Console.ReadLine()
+    'End Sub
+
+    'Constructor Chaining
+    'Class User
+    '    Public Sub New()
+    '        Console.Write("Hi, ")
+    '    End Sub
+    '    Public Sub New(ByVal a As String)
+    '        Me.New()
+    '        Console.Write(a)
+    '    End Sub
+    '    Public Sub New(ByVal a As String, ByVal b As String)
+    '        Me.New("welcome")
+    '        Console.Write(a & " " & b)
+    '    End Sub
+    'End Class
+
+    'Sub Main()
+    '    Dim user As User = New User(" to ", "VietNam")
+    '    Console.WriteLine()
+    '    Console.WriteLine("Press Enter Key to Exit...")
+    '    Console.ReadLine()
+    'End Sub
+
+    'Destructor
+
+    Class User
+        Public Sub New()
+            Console.WriteLine("An Instance of class created")
+        End Sub
+        Protected Overrides Sub Finalize()
+            Console.WriteLine("An Instance of class destroyed")
+        End Sub
+    End Class
+    Sub Main()
+        Details()
+        GC.Collect()
+        Console.ReadLine()
+    End Sub
+    Public Sub Details()
+        Dim user As User = New User()
+    End Sub
+End Module
---
+++ Test_VB_3/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(135, 31)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(323, 31)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(635, 338)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+End Class
